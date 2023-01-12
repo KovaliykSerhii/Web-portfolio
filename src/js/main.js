c5b10e30e814471e5e4a6b6182fe146b2cd64af1
Serhii <kovaliykserhii@gmail.com>
2023-01-12 15:04:26 +0200

    new btn

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,9 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
-}
+};
 
+function soon(){
+    var name = document.getElementById("name");
+    var displayName = 'Link will be soon';
+    alert(displayName);
+};
