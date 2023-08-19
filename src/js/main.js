468900910610c85a52fc29b2fd8c9c107fba8c09
Serhii <110421345+KovaliykSerhii@users.noreply.github.com>
2023-08-19 09:54:32 -0400

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
+// Mobile hamburger header
+
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 };
 
+// Button for DDLC in experience section 
 function soon(){
     var name = document.getElementById("name");
     var displayName = 'Link will be soon';
